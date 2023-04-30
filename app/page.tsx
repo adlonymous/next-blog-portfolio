4862d58cfde1ff8714aa0100a9335bf6907d0978
adlonymous <adlonymous@gmail.com>
2023-04-30 11:28:27 +0530

    Second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
-
+import Posts from "./components/Posts"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2">
-      <p className="mt-12 mb-12 text-3xl text-center dark:text-white font-extralight">
+      <p className="mt-12 text-3xl text-center dark:text-white font-extralight">
       👋 &nbsp; Hey There! &nbsp;
         <span className="whitespace-nowrap">
           I'm  <span className="pl-1.5 font-extrabold tracking-wider subpixel-antialiased italic underline decoration-sky-500 decoration-wavy underline-offset-2">adlonymous</span>.
         </span>
       </p>
+      <Posts />
     </main>
   )
 }
