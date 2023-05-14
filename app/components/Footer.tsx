c907578d59b22d77222103a71a267865de4ac2ec
adlonymous <adlonymous@gmail.com>
2023-05-14 17:07:56 +0530

    Added On-demand revalidation

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     <footer>
         <div className="flex flex-col justify-center items-center p-4 bg-neutral-800">
             <p className="text-gray-500 dark:text-sky-500 text-m">
-                Made with 💙 by adlonymous © 2023
+                Made with 💙 by adlonymous © 2023 
             </p>
         </div>
     </footer>
--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 
 export default function MyProfilePic() {
+  
   return (
     <section className="w-full mx-auto pt-1">
         <Image className="border-4 border-black dark:border-sky-500 drop-shadow-xl shadow-black rounded-t-full mx-auto mt-6"
