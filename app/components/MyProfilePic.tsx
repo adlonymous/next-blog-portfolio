97414f903f90f259b356b770994e5ff6aac7e3a2
Aadil Ahmed <adlonymous@gmail.com>
2023-05-16 14:22:44 +0000

    Changed pfp

--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -5,7 +5,7 @@ export default function MyProfilePic() {
   return (
     <section className="w-full mx-auto pt-1">
         <Image className="border-4 border-black dark:border-sky-500 drop-shadow-xl shadow-black rounded-t-full mx-auto mt-6"
-            src="/images/monkerejectnft.png" width={200} height={200} alt="adlonymous" priority={true}/>
+            src="/images/rivals.png" width={200} height={200} alt="adlonymous" priority={true}/>
     </section>
   )
 }
