30a8ffa6ec90976c428ea0aed528795b4334a2a1
adlonymous <adlonymous@gmail.com>
2024-01-30 19:24:53 -0500

    Added MDX handling on the frontend

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -1,17 +1,23 @@
-import { getSortedPostsData } from "@/lib/posts"
-import ListItem from "./ListItem"
+import { getPostsMeta } from "@/lib/posts";
+import ListItem from "./ListItem";
 
-export default function Posts() {
-    const posts = getSortedPostsData()
+export default async function Posts() {
+  const posts = await getPostsMeta();
 
-    return (
-        <section className="mt-12 mx-auto max-w-2xl">
-            <h2 className="text-4xl font-bold dark:text-white/90 underline decoration-dashed decoration-sky-500 underline-offset-2">My Blog</h2>
-            <ul className="w-full indent-8">
-                {posts.map(post => (
-                    <ListItem key={post.id} post={post} />
-                ))}
-            </ul>
-        </section>
-    )
+  if (!posts) {
+    return <p className="mt-10 text-center">Sorry, no posts available</p>;
+  }
+
+  return (
+    <section className="mt-12 mx-auto max-w-2xl">
+      <h2 className="text-4xl font-bold dark:text-white/90 underline decoration-dashed decoration-sky-500 underline-offset-2">
+        My Blog
+      </h2>
+      <ul className="w-full indent-8 list-none p-0">
+        {posts.map((post) => (
+          <ListItem key={post.id} post={post} />
+        ))}
+      </ul>
+    </section>
+  );
 }
